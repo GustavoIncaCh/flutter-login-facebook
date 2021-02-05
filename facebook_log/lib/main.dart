4fc778055bf4530812c40990c251ead6496d0258
naniwa9 <tavasoxd@gmail.com>
2021-02-05 13:03:00 -0500

    login added

---
+++ facebook_log/lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as JSON;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'login facebook',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Facebook login'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isloged = false;
+  Map userData;
+  final facebookLogin = FacebookLogin();
+
+  _loginFB() async {
+    final result = await facebookLogin.logInWithReadPermissions(['email']);
+
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final token = result.accessToken.token;
+        final graphResponse = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,picture,email&access_token=${token}');
+        final profile = JSON.jsonDecode(graphResponse.body);
+        print(profile);
+        setState(() {
+          userData = profile;
+          isloged = true;
+        });
+
+        break;
+
+      case FacebookLoginStatus.cancelledByUser:
+        setState(() => isloged = false);
+        break;
+      case FacebookLoginStatus.error:
+        setState(() => isloged = false);
+        break;
+    }
+  }
+
+  _logoutFB() {
+    facebookLogin.logOut();
+    setState(() {
+      isloged = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return MaterialApp(
+      home: Scaffold(
+          body: Center(
+              child: isloged
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.network(
+                          userData["picture"]["data"]["url"],
+                          height: 50.0,
+                          width: 50.0,
+                        ),
+                        Text(userData["name"]),
+                        Text(userData["email"]),
+                        OutlineButton(
+                          child: Text("logout"),
+                          onPressed: () {
+                            _logoutFB();
+                          },
+                        )
+                      ],
+                    )
+                  : OutlineButton(
+                      child: Text("login de Facebook"),
+                      onPressed: () {
+                        _loginFB();
+                      },
+                    ))),
+    );
+  }
+}
